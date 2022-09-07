46157cb9577aad11aee268a58091e0348ff5e2a6
AlexNurzhanov <turk.alek@yandex.ru>
2022-09-07 10:48:56 +0400

    создал папку zad_038 (Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.)

---
+++ zad_038/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+
+int size = new Random().Next(1, 11);
+
+double[] Array(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble()*10;
+    }
+    return array;
+}
+
+void MinMax(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min();
+    System.Console.WriteLine($"Минимальное значение массива {arr.Min()}, максимальное значение массива {arr.Max()}");
+    System.Console.WriteLine($"Разница между максимальным  и минимальным значениями массива составляет {difference}");
+    System.Console.WriteLine();
+}
+
+MinMax(Array(size));
