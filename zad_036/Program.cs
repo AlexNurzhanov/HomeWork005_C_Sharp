5efb3ce4b5090ec715e7c90175abd62e86a595e7
AlexNurzhanov <turk.alek@yandex.ru>
2022-09-07 10:11:19 +0400

    создал папку zad_036 в ней выполнено задание: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.

---
+++ zad_036/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] GetArray(int size = 4)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++){
+        arr[i] = new Random().Next(1, 10);
+    }
+    return arr;
+}
+
+
+int FindSumOddI(int[] array)
+{
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+    {
+    if (i % 2 > 0)
+{
+    sum = sum + array[i];
+}
+    }
+
+    return sum;
+}
+int[] array = GetArray();
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(FindSumOddI(array));
